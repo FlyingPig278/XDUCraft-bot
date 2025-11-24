a6ede359e0b2b48c92c4b8198cb18468aa928246
FlyingPig278 <flyingpig278@gmail.com>
2025-11-24 18:03:26 +0800

    feat: 实现私聊导入并更新命令

--- xducraft_bot/plugins/xducraft_mc_status/__init__.py
+++ xducraft_bot/plugins/xducraft_mc_status/__init__.py
@@ -1,17 +1,30 @@
 from nonebot import on_command
-from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent
+from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent, PrivateMessageEvent
 from nonebot.internal.adapter import Message
 from nonebot.params import CommandArg
 from nonebot.plugin import PluginMetadata
 
 
 # 从 handlers 导入子命令处理逻辑
-from .handlers import SUBCOMMAND_HANDLERS, handle_query_all, handle_query_single
+from .handlers import SUBCOMMAND_HANDLERS, handle_query_all, handle_query_single, handle_private_import
 
 # 从 data_manager 导入需要在主命令中直接使用的函数
 from .data_manager import get_show_offline_by_default
 
-mc_status = on_command("mcs", aliases={"mcstatus", "服务器", "状态"}, block=True)
+mc_status = on_command("mcs", aliases={"mcstatus", "服务器", "状态","list"}, block=True)
+mc_status_private = on_command("mcs", aliases={"mcstatus"}, priority=4, block=True)
+
+
+# 私聊命令处理
+@mc_status_private.handle()
+async def handle_private_command(bot: Bot, event: PrivateMessageEvent, args: Message = CommandArg()):
+    arg_list = args.extract_plain_text().strip().split()
+    if arg_list and arg_list[0].lower() == 'import':
+        await handle_private_import(bot, event, arg_list)
+    else:
+        # Optional: handle other private commands or give a generic message
+        await mc_status_private.finish("私聊中仅支持 'import' 操作。")
+
 
 __plugin_meta__ = PluginMetadata(
     name="XDUCraft_mc_status",
@@ -24,7 +37,6 @@ __plugin_meta__ = PluginMetadata(
 
 【管理员命令】
 /mcs edit: 获取配置链接以在Web UI中编辑
-/mcs import <压缩字符串>: 从Web UI或备份中导入配置
 /mcs add <IP>: 快速添加服务器
 /mcs remove <IP>: 快速移除服务器
 ...更多命令请使用 /mcs help 查看""",
